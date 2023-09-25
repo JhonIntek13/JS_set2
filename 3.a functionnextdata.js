5408294de2c84cb49a32bff933f3862bb474601f
JhonIntek13 <136015556+JhonIntek13@users.noreply.github.com>
2023-09-25 15:55:40 -0600

    fix set 2

--- 3.a functionnextdata.js
+++ 3.a functionnextdata.js
@@ -1,16 +1,27 @@
 function nextdata(str) {
-  return str.replace(/[a-zA-Z]/g, function (match) {
-    var codePoint = match.charCodeAt(0) + 1;
+  return str.replace(/[a-zA-Z0-9]/g, function (match) {
+    if (/[a-zA-Z]/.test(match)) {
+      var codePoint = match.charCodeAt(0) + 1;
 
-    if (codePoint > 90 && codePoint < 97) {
-      codePoint = 65;
-    } else if (codePoint > 122) {
-      codePoint = 97;
+      if (codePoint > 90 && codePoint < 97) {
+        codePoint = 65;
+      } else if (codePoint > 122) {
+        codePoint = 97;
+      }
+
+      return String.fromCharCode(codePoint);
+    } else if (/[0-9]/.test(match)) {
+      // Shift numbers cyclically by one
+      var num = parseInt(match);
+      if (num === 9) {
+        return '0';
+      } else {
+        return (num + 1).toString();
+      }
     }
-
-    return String.fromCharCode(codePoint);
   });
 }
 
 console.log(nextdata('aBc')); // Output: "bCd"
-console.log(nextdata('aK89')); // Output: "bL89"
+console.log(nextdata('aK89')); // Output: "bL90"
+console.log(nextdata('xyz'));
